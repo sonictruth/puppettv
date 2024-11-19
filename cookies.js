99fe1d6264f0d08c24b43057766ee716cc369836
Alexandru Constantin <dextor@gmail.com>
2024-11-19 10:04:51 +0100

    Create cookies.js

---
+++ cookies.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        name: 'DOSESSV3PRI',
+        value: 'xxxxxxxxxxxxxxx',
+        domain: '.digionline.ro',
+        httpOnly: true,
+        secure: true,
+        sameSite: 'Strict'
+    },
+    {
+        name: 'deviceId',
+        value: 'xxxxxxxxxxxxxxx.xxxxxxxxxxxxxxx',
+        domain: '.digionline.ro',
+        httpOnly: false,
+        secure: false,
+    }
+]
